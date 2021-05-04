21c1fa58d38ff8f46b3041f08dc500bfe273f33c
Dalen Chan <dalenchan@dalens-mbp.lan>
2021-05-04 22:55:42 +0100

    Added JS - Form input renders on page

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 // Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 // Function 
+
+function addTodo(event){
+    // Prevent form submitting
+    event.preventDefault();
+
+    // Todo div 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    
+    // Li 
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // Completed check
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+
+    // Delete Button 
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    // Append to list 
+    todoList.appendChild(todoDiv);
+
+    // Clear Input 
+    todoInput.value = "";
+}
